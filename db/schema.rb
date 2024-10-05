04f4e0324a78fa320a334a55fdd50798aeb58163
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-10-06 00:33:16 +0100

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_04_171550) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin", default: false
+    t.boolean "admin"
     t.integer "organization_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
