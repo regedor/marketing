ccc8ff11ab5abcf00d45eddc8fff2bf9e19a2170
JoseBambora <zebragapt@gmail.com>
2024-10-06 14:30:53 +0100

    Added the feature to remove teams.

--- app/controllers/dashboard/teams_controller.rb
+++ app/controllers/dashboard/teams_controller.rb
@@ -1,10 +1,10 @@
 class Dashboard::TeamsController < ApplicationController
+  before_action :set_organization
   before_action :authenticate_user!
   before_action :authorize_leader!
-  before_action :authorized_organization! , only: %i[ create ]
+  before_action :authorized_organization!, only: %i[ create ]
 
   def create
-    @organization = Organization.find(params[:organization_id])
     @team = @organization.teams.new(team_params)
     respond_to do |format|
       if @team.save
@@ -17,7 +17,26 @@ class Dashboard::TeamsController < ApplicationController
     end
   end
 
+  def destroy
+    @team = Team.find(params[:id])
+    if @team.destroy
+      respond_to do |format|
+        format.html { redirect_to dashboard_organization_path(@organization), status: :see_other, notice: "Team was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to dashboard_organization_path(@organization), status: :unprocessable_entity, alert: "Failed to destroy the team." }
+        format.json { render json: @team.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
+    def set_organization
+      @organization = Organization.find(params[:organization_id])
+    end
+
     def team_params
       params.require(:team).permit(:name)
     end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :organization
   validates :name, presence: true, uniqueness: { scope: :organization_id }
+  has_many :teams_users, dependent: :destroy
+  has_many :users, through: :teams_users
 end
