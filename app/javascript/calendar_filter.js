78b60b3efcd47690c7e194d6335f5a2e296fb3dc
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-10-24 22:49:11 +0100

    Use turbo for link nav

--- app/javascript/calendar_filter.js
+++ app/javascript/calendar_filter.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
+function initializeCalendarFilter() {
     const calendarFilterForm = document.getElementById("calendar_filter_form");
     if (calendarFilterForm) {
       calendarFilterForm.addEventListener("change", () => {
         calendarFilterForm.submit();
       });
     }
-  });
+  }
+  
+  document.addEventListener("turbo:load", initializeCalendarFilter);
+  document.addEventListener("DOMContentLoaded", initializeCalendarFilter);
