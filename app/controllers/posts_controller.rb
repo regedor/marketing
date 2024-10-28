8f171eb2807b45b2417000abf612a03f0165aaf5
José Carvalho <73351680+JoseBambora@users.noreply.github.com>
2024-10-28 15:11:55 +0000

    Posts Images (#11)
    * Download imagees and filename validation is already correct
    
    * Added the logic to change filenames.
    
    * URL validation
    
    * Fixed minor error with warning boxes.
    
    * Edit filename
    
    * Fixed cancel button
    
    ---------
    
    Co-authored-by: zeisalone <jmbarbosa2002@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_calendar
   before_action :check_organization!
-  before_action :set_post, only: [ :show, :edit, :update, :destroy, :update_design_idea ]
+  before_action :set_post, only: [ :show, :edit, :update, :destroy, :update_design_idea, :download ]
   before_action :check_author!, only: [ :edit, :update, :destroy, :update_design_idea ]
   before_action :sanitize_categories, only: [ :create, :update ]
 
@@ -56,6 +56,20 @@ class PostsController < ApplicationController
   # PATCH /calendars/:calendar_id/posts/:id/update_design_idea
   def update_design_idea
     @post.update(perspective_params_design_idea)
+  end
+
+  # GET /calendars/:calendar_id/posts/:id/download
+  def download
+    attachments = @post.perspectives.map { |p| p.attachments }.flatten.select { |a| a.status == "approved" }.reject { |a| a.type_content == "cloud" }
+    zip_filename = "#{@post.id}_attachments.zip"
+    zip_data = Zip::OutputStream.write_buffer do |zip|
+      attachments.each do |attachment|
+        zip.put_next_entry(attachment.filename)
+        zip.print attachment.content
+      end
+    end
+    zip_data.rewind
+    send_data zip_data.read, filename: zip_filename, type: "application/zip", disposition: "attachment"
   end
 
   private
