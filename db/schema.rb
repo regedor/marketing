39066171a2a5e0a53e6a621a52ebb26ddbc6a0d5
JoseBambora <zebragapt@gmail.com>
2024-11-05 10:38:18 +0000

    Added Log Entries table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_23_141020) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_24_132239) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -79,13 +79,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_23_141020) do
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
-  
+
   create_table "log_entries", force: :cascade do |t|
     t.string "controller_name"
     t.text "info"
     t.datetime "created_at", null: false
   end
-  
+
   create_table "organizations", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
