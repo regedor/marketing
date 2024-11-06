b7743f1ad4a068cca1f62c21f6ab0d059f8a5e8a
Pedro Braga <peradi2001@gmail.com>
2024-11-06 16:09:26 +0000

    Remove CSS Compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Do not fall back to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.digest = true
-  config.assets.css_compressor = :sass
+  config.assets.css_compressor = nil
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
