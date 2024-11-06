589380a3f9d4048124e1e993f7ced266b6dc9337
Pedro Braga <peradi2001@gmail.com>
2024-11-06 16:39:04 +0000

    Asset cache reset before precompilation

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 # Precompile the assets
 echo "Precompiling assets..."
+bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 
 # Check if precompilation was successful
 if [ $? -eq 0 ]; then
   echo "Assets precompiled successfully."
   echo "Starting Rails server..."
-  # Start the Rails server
+  
   bundle exec rails server
 else
   echo "Asset precompilation failed. Rails server not started."
