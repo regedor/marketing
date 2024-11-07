2e6d319559db99fb337c5f35e48cdbdb710e95e5
JoseBambora <zebragapt@gmail.com>
2024-11-07 20:07:05 +0000

    Added pipelines to the settings page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,7 @@ class DashboardController < ApplicationController
   def index
     @users = User.where(organization_id: current_user.organization_id)
     @calendars = Calendar.where(organization_id: current_user.organization_id)
+    @pipelines = Pipeline.where(organization_id: current_user.organization_id)
   end
 
   private
