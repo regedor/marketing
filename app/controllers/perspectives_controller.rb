9d55d5e96a37eaae4e648647a2459b8216c068b9
JoseBambora <zebragapt@gmail.com>
2024-11-12 15:26:54 +0000

    Fixed minor error related with perspective show view. Also made some improvements to calendar page.

--- app/controllers/perspectives_controller.rb
+++ app/controllers/perspectives_controller.rb
@@ -15,6 +15,7 @@ class PerspectivesController < ApplicationController
     new_perspectives = Socialplatform.all.reject { |socialplatform| post_socialplatforms.include?(socialplatform) }.map { |s| Perspective.new(socialplatform: s) }
     @perspectives = (post_perspectives + new_perspectives).sort_by { |perspective| perspective.socialplatform.present? ? perspective.socialplatform&.name : "Default" }
     @publishplatform = Publishplatform.where(post: @post).map { |pp| pp.socialplatform }
+    @attachments = @post.perspectives.flat_map(&:attachments).select { |a| a.filename.present? && a.id.present? }.uniq { |a| a.filename }.sort_by { |a| a.filename.to_s.downcase }
   end
 
   # POST /calendars/:calendar_id/posts/:post_id/perspectives
